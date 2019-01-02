47784554e4b67fad754500fc4b10cef7c16005b0
Kausika <Kausika@DESKTOP-8RSEAS0.Dlink>
2019-01-02 21:06:34 +0530

    My first Maven project on Git

---
+++ src/main/java/generic/AutoConstant.java
@@ -0,0 +1,10 @@
+package generic;
+
+public interface AutoConstant 
+{
+     String chrome_key = "webdriver.chrome.driver";
+     String chrome_value = "./src/main/resources/chromedriver.exe";
+     String gecko_key = "webdriver.gecko.driver";
+     String gecko_value = "./src/main/resources/geckodriver.exe";
+     String file_path = "./src/test/resources/input.xlsx";
+}
